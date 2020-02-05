404411fd48e6174504e43f8029b06f8a3b0fe092
chris <chris@chris-pc>
2020-02-05 15:24:00 -0600

    First commit for lab 4

---
+++ cat.py
@@ -0,0 +1,39 @@
+from peewee import * 
+
+# Which database?
+db = SqliteDatabase('cats.sqlite')
+
+# Create a Model class. This defines both the fields in the objects in your program
+# and also the columns in the database.  Peewee maps between the two.
+
+class Cat(Model):
+    name = CharField()
+    color = CharField()
+    age = IntegerField()
+
+    # Link this model to a particular database
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'{self.name} is a {self.color} cat and is {self.age} years old'
+
+    
+# Connect to DB,, and create tables that map to the model Cat.
+# Can have many models, create_tables takes a list of model classes as the argument
+db.connect()
+db.create_tables([Cat])
+
+# Create Cat objects and call save function to insert them into the database
+print('\nCreate and save 3 cats')
+
+zoe = Cat(name="Zoe", color='Ginger', age=3)
+zoe.save()
+
+holly = Cat(name="Holly", color='Tabby', age=7)
+holly.save()
+
+fluffy = Cat(name="Fluffy", color='Black', age=1)
+fluffy.save()
+
+
---
+++ hello_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+# Creates or opens connection to db file
+conn = sqlite3.connect('first_db.sqlite')
+
+# Create a table
+conn.execute('create table phones(brand text, version integer)')
+
+# Add some data
+conn.execute('insert into phones values ("Android", 5)')
+conn.execute('insert into phones values ("iPhone", 6)')
+
+conn.commit() # Finalize updates
+
+# Execute a query. execute() returns a cursor
+# Can use the cursor in a loop to read each row in turn
+for row in conn.execute('select * from phones'):
+    print(row)
+
+#conn.execute('drop table phones') # Delete table
+
+conn.commit() # Ask the database to save changes - dont forget!
+
+conn.close() # And close connection.
